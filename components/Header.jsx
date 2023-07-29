ae30a6585949c9ed579449c2ec306e28c584c352
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-07-29 19:52:21 +0530

    Customers & orders pages  added

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='flex justify-between px-4 pt-3 text-lg'>
       <h2 className='font-bold text-purple-800'>Dashboard</h2>
-      <h2 className='font-semibold text-gray-600'>Welcome back, Aish</h2>
+      <h2 className='font-semibold text-gray-700'>Welcome back, Aish</h2>
     </div>
   )
 }

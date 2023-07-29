8e63064be44c3984378c0fc1b7c3547003172fed
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-07-29 18:39:39 +0530

    Recent revenue added

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='flex justify-between px-4 pt-4'>
-      <h2>Dashboard</h2>
-      <h2>Welcome back, Aish</h2>
+    <div className='flex justify-between px-4 pt-3 text-lg'>
+      <h2 className='font-bold text-purple-800'>Dashboard</h2>
+      <h2 className='font-semibold text-gray-600'>Welcome back, Aish</h2>
     </div>
   )
 }
